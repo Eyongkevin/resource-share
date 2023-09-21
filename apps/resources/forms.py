74820e52f8a41be3d0a21e5de599da5d836f1882
Vitalii Sili <vitaliisili@yahoo.com>
2023-09-21 21:02:43 +0200

    👷 Add or update CI build system.

--- apps/resources/forms.py
+++ apps/resources/forms.py
@@ -13,3 +13,11 @@ class PostResourceForm(forms.Form):
     link = forms.URLField(widget=forms.TextInput(attrs={'class': 'field'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'field'}), )
     tags = forms.MultipleChoiceField(choices=get_tags, widget=forms.CheckboxSelectMultiple)
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['tags'].choices = self.get_tags()
+    #
+    # def get_tags(self):
+    #     options = ((tag.id, tag.name) for tag in Tag.objects.all())
+    #     return options
