2ab6a0a0eee7b1d4ad8b63aeea05fc1ff6d88583
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2023-09-22 10:24:27 +0100

    added a comment on the view

--- apps/user/views.py
+++ apps/user/views.py
@@ -8,19 +8,21 @@ from apps.user.models import User
 
 
 class UserListView(LoginRequiredMixin, ListView):
+    """Class based views are the best for others"""
+
     model = User
     template_name = "user/user-list.html"
-    context_object_name = 'user_list'
+    context_object_name = "user_list"
 
 
 class UserAccountView(LoginRequiredMixin, TemplateView):
-    template_name = 'user/user-account.html'
+    template_name = "user/user-account.html"
 
 
 class SignUpView(CreateView):
     form_class = SignUpForm
-    template_name = 'user/sign-up.html'
-    success_url = reverse_lazy('user-account')
+    template_name = "user/sign-up.html"
+    success_url = reverse_lazy("user-account")
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -29,10 +31,10 @@ class SignUpView(CreateView):
 
 
 class SignInView(LoginView):
-    login = 'sign-in'
-    template_name = 'user/sign-in.html'
+    login = "sign-in"
+    template_name = "user/sign-in.html"
     authentication_form = SignInForm
 
 
 class UserLogoutView(LogoutView):
-    logout = 'logout'
+    logout = "logout"
